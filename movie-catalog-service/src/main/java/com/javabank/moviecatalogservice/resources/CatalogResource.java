b2cf97805a7c10ca2fd82b2615ad7eb91d164077
biddut.bhowmic <biddut.bhowmic@gmail.com>
2020-04-19 03:07:01 -0400

    Configure all server with discovery server

--- movie-catalog-service/src/main/java/com/javabank/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/javabank/moviecatalogservice/resources/CatalogResource.java
@@ -1,24 +1,54 @@
 package com.javabank.moviecatalogservice.resources;
 
+
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import com.javabank.moviecatalogservice.models.CatalogItem;
+import com.javabank.moviecatalogservice.models.UserRating;
+import com.javabank.moviecatalogservice.models.Movie;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
 	
-	@RequestMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		
-		return Collections.singletonList(new CatalogItem("Transformers","Test",4));
-		
-		
-	}
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+
+        return userRating.getRatings().stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+    }
+	
+	/*
+	 * @RequestMapping("/{userId}") public List<CatalogItem>
+	 * getCatalog(@PathVariable("userId") String userId){
+	 * 
+	 * return Collections.singletonList(new CatalogItem("Transformers","Test",4));
+	 * 
+	 * 
+	 * }
+	 */
 	
 	
    /* public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
